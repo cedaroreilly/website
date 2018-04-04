bd9d2d94ec7a62cf89eb4d07c194141be9648ae3
Aaron Panchal <aaron.panchal@appfolio.com>
2018-04-03 21:38:35 -0700

    Add html-webpack-plugin to compile html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,8 +10,17 @@ module.exports = {
   },
   mode: process.env.NODE_ENV,
   plugins: [
-    new CopyWebpackPlugin([
-      { from: 'src/favicons' }
-    ])
+    new CopyWebpackPlugin([{ from: 'src/favicons' }]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              minifyCSS: true,
+              collapseWhitespace: true,
+              removeComments: true
+            }
+          : false
+    })
   ]
 };
